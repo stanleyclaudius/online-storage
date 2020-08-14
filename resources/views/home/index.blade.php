478ed206ef89669ca740783ea3e69813cc9a827c
Stanley Claudius <stanleyclaudiuss@gmail.com>
2020-08-14 10:50:24 +0700

    Create template for home page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -3,5 +3,45 @@
 @section('title', 'Online Storage')
 
 @section('content')
+<div class="grid grid-cols-2 gap-16 items-center mb-24">
+	<div>
+		<p class="text-4xl">Your Documents Means Alot</p>
+		<p class="text-sm mb-6 text-gray-500">Backup your documents online, so you no need to worry about losing data at your device. We keep your data safe at our site. We're here for you.</p>
+		<p class="mb-10 text-gray-700">We provide an online storage for you to save your documents online. So whenever you have an important document, you can save at our site for backup, and it's safe also free. You can access from every platform because it's online. So when you lost your documents, you can fetch from our site.</p>
+		<a href="" class="rounded px-3 py-2 bg-blue-500 text-white transition ease-in-out duration-150 hover:bg-blue-600">Explore More</a>
+	</div>
+	<div>
+		<img src="{{ asset('img') }}/content/jumbotron.svg" alt="Online Storage">
+	</div>
+</div>
 
+<section id="why" class="mb-24">
+	<p class="text-4xl text-center">Why Choose Us?</p>
+	<p class="text-center text-gray-500 mt-1">List of benefits that you can got from our storage service</p>
+	<div class="grid grid-cols-4 gap-10 items-center mt-10">
+		<div class="w-full h-64 bg-gray-200"></div>
+		<div class="w-full h-64 bg-gray-200"></div>
+		<div class="w-full h-64 bg-gray-200"></div>
+		<div class="w-full h-64 bg-gray-200"></div>
+	</div>
+</section>
+
+<section id="faq" class="mb-24">
+	<p class="text-4xl text-center">General FAQ</p>
+	<p class="text-center text-gray-500 mt-1">General questions about our service</p>
+	<div class="grid grid-cols-2 gap-16 items-center mt-10">
+		<div>
+			<div class="w-full h-12 border border-gray-400"></div>
+			<div class="w-full h-12 border border-gray-400 mt-10"></div>
+			<div class="w-full h-12 border border-gray-400 mt-10"></div>
+			<div class="w-full h-12 border border-gray-400 mt-10"></div>
+		</div>
+		<div>
+			<div class="w-full h-12 border border-gray-400"></div>
+			<div class="w-full h-12 border border-gray-400 mt-10"></div>
+			<div class="w-full h-12 border border-gray-400 mt-10"></div>
+			<div class="w-full h-12 border border-gray-400 mt-10"></div>
+		</div>
+	</div>
+</section>
 @endsection
--- resources/views/template/main.blade.php
+++ resources/views/template/main.blade.php
@@ -8,14 +8,14 @@
 </head>
 <body>
 
-    <nav class="shadow-lg flex items-center w-full h-16">
+    <nav class="shadow-lg flex items-center w-full h-16 mb-16">
         <div class="container mx-auto flex items-center justify-between px-5">
             <div>
                 <a href="/" class="text-xl">Online Storage</a>
             </div>
             <ul class="flex items-center">
                 <li><a href="" class="mr-10">Sign In</a></li>
-                <li><a href="" class="rounded px-3 py-2 bg-blue-500 text-white">Sign Up</a></li>
+                <li><a href="" class="rounded px-3 py-2 bg-blue-500 text-white transition duration-150 ease-in-out hover:bg-blue-600">Sign Up</a></li>
             </ul>
         </div>
     </nav>
@@ -23,6 +23,47 @@
     <div class="container mx-auto px-5">
         @yield('content')
     </div>
+
+    <footer class="bg-gray-900 py-8">
+        <div class="container mx-auto px-5">
+            <div class="grid grid-cols-2 gap-24">
+                <div>
+                    <p class="text-4xl text-white">Online Storage</p>
+                    <p class="text-gray-300 mt-2">Website that provide storage services for user.</p>
+                    <p class="text-white mt-6 text-lg">Primary Features</p>
+                    <ul class="mt-3">
+                        <li class="text-white flex items-center">
+                            <img src="{{ asset('icon') }}/storage.png" alt="Online Storage" width="18" class="mr-3">
+                            Secondary Storage
+                        </li>
+                        <li class="text-white flex items-center mt-2">
+                            <img src="{{ asset('icon') }}/backup.png" alt="Online Storage" width="18" class="mr-3">
+                            Backup Your Documents
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <p class="text-4xl text-white">Website Copyright</p>
+                    <p class="text-gray-300 mt-2">This website is made for educational purpose, use it wisely.</p>
+                    <p class="text-white mt-6 text-lg">Website creator social page</p>
+                    <ul class="mt-3">
+                        <li class="text-white">
+                            <a href="https://instagram.com/stanleyclaudius" target="_blank" class="flex items-center text-lg">
+                                <img src="{{ asset('icon') }}/instagram.png" alt="Online Storage" width="18" class="mr-3">
+                                stanleyclaudius
+                            </a>
+                        </li>
+                        <li class="text-white mt-2">
+                            <a href="https://github.com/stanleyclaudius" target="_blank" class="flex items-center text-lg">
+                                <img src="{{ asset('icon') }}/github.png" alt="Online Storage" width="18" class="mr-3">
+                                stanleyclaudius
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
     
 @yield('script')
 </body>
